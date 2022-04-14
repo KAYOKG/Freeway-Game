8032281f8fdea7af8040e119cdbbaa3e6d5434e4
KAYOKG <kayo.kg@hotmail.com>
2022-04-14 12:55:26 -0300

    Final Conclusion

--- sketch.js
+++ sketch.js
@@ -6,10 +6,15 @@ let car3Image
 let characterImage
 let xCharacter = 100
 let yCharacter = 366
+// variavel dos pontos
+let myPoints = 0
 //collision variables
 let collision = false
 let roadImage
-
+// musiquinha do jogo 
+let soundTrack
+let collisionSound
+let scoreSound
 function preload(){
   roadImage = loadImage('images/road.png')
   characterImage = loadImage('images/character.png')
@@ -17,9 +22,13 @@ function preload(){
   car2Image = loadImage('images/car 2.png')
   car3Image = loadImage('images/car 3.png')
   carsImage = [car1Image, car2Image, car3Image, car1Image, car2Image, car3Image]
+  collisionSound = loadSound('sons/collisionSound.mp3')
+  soundTrack = loadSound('sons/soundtrack.mp3')
+  scoreSound = loadSound('sons/scores.wav')
 }
 function setup() {
   createCanvas(500, 400)
+  soundTrack.loop()
 }
 function draw() {
   background(roadImage)
@@ -29,6 +38,8 @@ function draw() {
   character_movement()
   car_movement_loop()
   collide_check()
+  score()
+  scoring()
 }
 function show_character(){
   image(characterImage, xCharacter, yCharacter, 30, 30)
@@ -45,10 +56,25 @@ function collide_check(){
   for(i = 0; i < carsImage.length; i++){
     collision = collideRectCircle(xCars[i], yCars[i], carsLength, carsheight, xCharacter, yCharacter, 15)
     if (collision){
+      collisionSound.play()
       hit()
     }
   }
 }
 function hit(){
   yCharacter = 366 
+  myPoints = 0
+}
+function score(){
+  textAlign(CENTER)
+  textSize(35)
+  fill(255,0,0)
+  text(myPoints, width/10, 30)
+}
+function scoring(){
+  if (yCharacter < 5){
+    scoreSound.play()
+    myPoints += 1
+    yCharacter = 366
+  }
 }
